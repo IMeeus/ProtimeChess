446c6747f72f5b0ac27ef4ce3da7a4bf9cada0ab
IMeeus <a.meeus@flanderijn.nl>
2023-01-04 09:56:26 +0100

    Finish Dummy API

---
+++ Server/API/Controllers/ChessController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/chess")]
+    public class ChessController : ControllerBase
+    {
+        [HttpGet("test")]
+        public IActionResult Test()
+        {
+            return Ok();
+        }
+    }
+}
